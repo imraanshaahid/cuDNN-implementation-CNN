dfcc23ae256b653030f45635c9003a4f38711dfa
imraanshaahid <imraan.shaahid@gmail.com>
2021-12-30 19:52:43 -0800

    Create neural_implementation.c

---
+++ neural_implementation.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "include/data_file.h"
+#include "include/neural.h"
+
+#define PIXEL_SCALE(x) (((float) (x)) / 255.0f)
+
+#define RAND_FLOAT() (((float) rand()) / ((float) RAND_MAX))
+
+void neural_network_random_weights(neural_network_t * network)
+{
+    int i, j;
+
+    for (i = 0; i < MNIST_LABELS; i++) {
+        network->b[i] = RAND_FLOAT();
+
+        for (j = 0; j < MNIST_IMAGE_SIZE; j++) {
+            network->W[i][j] = RAND_FLOAT();
+        }
+    }
+}
+
+void neural_network_softmax(float * activations, int length)
+{
+    int i;
+    float sum, max;
+
+    for (i = 1, max = activations[0]; i < length; i++) {
+        if (activations[i] > max) {
+            max = activations[i];
+        }
+    }
+
+    for (i = 0, sum = 0; i < length; i++) {
+        activations[i] = exp(activations[i] - max);
+        sum += activations[i];
+    }
+
+    for (i = 0; i < length; i++) {
+        activations[i] /= sum;
+    }
+}
+
+void neural_network_hypothesis(mnist_image_t * image, neural_network_t * network, float activations[MNIST_LABELS])
+{
+    int i, j;
+
+    for (i = 0; i < MNIST_LABELS; i++) {
+        activations[i] = network->b[i];
+
+        for (j = 0; j < MNIST_IMAGE_SIZE; j++) {
+            activations[i] += network->W[i][j] * PIXEL_SCALE(image->pixels[j]);
+        }
+    }
+
+    neural_network_softmax(activations, MNIST_LABELS);
+}
+
+float neural_network_gradient_update(mnist_image_t * image, neural_network_t * network, neural_network_gradient_t * gradient, uint8_t label)
+{
+    float activations[MNIST_LABELS];
+    float b_grad, W_grad;
+    int i, j;
+
+    neural_network_hypothesis(image, network, activations);
+
+    for (i = 0; i < MNIST_LABELS; i++) {
+        b_grad = (i == label) ? activations[i] - 1 : activations[i];
+
+        for (j = 0; j < MNIST_IMAGE_SIZE; j++) {
+            W_grad = b_grad * PIXEL_SCALE(image->pixels[j]);
+
+            gradient->W_grad[i][j] += W_grad;
+        }
+
+        gradient->b_grad[i] += b_grad;
+    }
+
+    return 0.0f - log(activations[label]);
+}
+
+float neural_network_training_step(mnist_dataset_t * dataset, neural_network_t * network, float learning_rate)
+{
+    neural_network_gradient_t gradient;
+    float total_loss;
+    int i, j;
+
+    memset(&gradient, 0, sizeof(neural_network_gradient_t));
+
+    for (i = 0, total_loss = 0; i < dataset->size; i++) {
+        total_loss += neural_network_gradient_update(&dataset->images[i], network, &gradient, dataset->labels[i]);
+    }
+
+    for (i = 0; i < MNIST_LABELS; i++) {
+        network->b[i] -= learning_rate * gradient.b_grad[i] / ((float) dataset->size);
+
+        for (j = 0; j < MNIST_IMAGE_SIZE + 1; j++) {
+            network->W[i][j] -= learning_rate * gradient.W_grad[i][j] / ((float) dataset->size);
+        }
+    }
+
+    return total_loss;
+}
